a6af0722013cfa9404de9948722672b0a47c16b3
James Hodgkinson <james@terminaloutcomes.com>
2026-03-07 12:42:51 +1000

    better error handling in the web endpoints

--- src/errors.rs
+++ src/errors.rs
@@ -2,15 +2,25 @@ use axum::response::IntoResponse;
 
 pub enum SiteError {
     NotFound,
-    InternalError(String),
+    Internal(String),
+    UnAuthorized(String),
+}
+
+impl SiteError {
+    pub fn internal(msg: String) -> Self {
+        SiteError::Internal(msg)
+    }
 }
 
 impl IntoResponse for SiteError {
     fn into_response(self) -> axum::response::Response {
         match self {
             SiteError::NotFound => (axum::http::StatusCode::NOT_FOUND, "Not Found").into_response(),
-            SiteError::InternalError(msg) => {
+            SiteError::Internal(msg) => {
                 (axum::http::StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+            }
+            SiteError::UnAuthorized(msg) => {
+                (axum::http::StatusCode::UNAUTHORIZED, msg).into_response()
             }
         }
     }
