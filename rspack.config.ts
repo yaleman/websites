25fb0636ba3d178d12ac59b62e274ac25f0820d2
James Hodgkinson <james@terminaloutcomes.com>
2026-03-07 12:38:07 +1000

    fixing JS build config

--- rspack.config.ts
+++ rspack.config.ts
@@ -5,10 +5,33 @@ export default defineConfig({
   entry: {
     editor: "./assets/admin/editor.ts",
   },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "builtin:swc-loader",
+            options: {
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, "admin-ui-assets"),
     filename: "[name].js",
     cssFilename: "[name].css",
+  },
+  performance: {
+    hints: false,
   },
   experiments: {
     css: true,
