c0d1b13a02d82e2351645d96b4eff003896d3fda
James Hodgkinson <james@terminaloutcomes.com>
2026-03-09 10:43:33 +1000

    tweaking caching in release/debug

--- src/middleware.rs
+++ src/middleware.rs
@@ -20,17 +20,43 @@ pub async fn log_requests(request: Request, next: Next) -> Response {
     res
 }
 
-pub(crate) async fn dont_cache_me(request: Request, next: Next) -> Response {
-    let mut response = next.run(request).await;
-    response.headers_mut().insert(
-        header::CACHE_CONTROL,
-        header::HeaderValue::from_static("no-store, no-cache, must-revalidate, proxy-revalidate"),
-    );
-    response
-        .headers_mut()
-        .insert(header::PRAGMA, header::HeaderValue::from_static("no-cache"));
-    response
-        .headers_mut()
-        .insert(header::EXPIRES, header::HeaderValue::from_static("0"));
-    response
+/// If you're in test/debug mode, then don't cache responses. Release mode might.
+///
+/// Relevant MDN links:
+///
+/// - [Cache-Control](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control)
+/// - [Pragma](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Pragma)
+/// - [Expires](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expires)
+///
+pub(crate) async fn set_cache(request: Request, next: Next) -> Response {
+    #[cfg(any(test, debug_assertions))]
+    {
+        let mut response = next.run(request).await;
+        response.headers_mut().insert(
+            header::CACHE_CONTROL,
+            header::HeaderValue::from_static(
+                "no-store, no-transform, no-cache, must-revalidate, proxy-revalidate",
+            ),
+        );
+        response
+            .headers_mut()
+            .insert(header::PRAGMA, header::HeaderValue::from_static("no-cache"));
+        response
+            .headers_mut()
+            .insert(header::EXPIRES, header::HeaderValue::from_static("0"));
+        response
+    }
+    #[cfg(not(any(test, debug_assertions)))]
+    {
+        let request_uri = request.uri().clone();
+        let mut response = next.run(request).await;
+        // because 404's might be fixed quick
+        if request_uri.path().starts_with("/assets/") && response.status().is_success() {
+            response.headers_mut().insert(
+                header::CACHE_CONTROL,
+                header::HeaderValue::from_static("private, max-age=300"),
+            );
+        }
+        response
+    }
 }
